9bb602d5dde303a59b24049c9e7adaf779649243
tshivanshu9 <tshivanshu9@gmail.com>
2025-07-05 17:13:49 +0530

    Express validator used

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const { body, validationResult } = require('express-validator');
 
-router.post('/', (req, res, next) => {
-    const user = User(req.body);
-    user.save();
-    res.send(req.body);
-    next();
+router.post('/', [
+    body('name').isLength({ min: 3 }),
+    body('email').isEmail(),
+    body('password').isLength({ min: 5 }),
+], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    }).then(user => {
+        res.json(user)
+    }).catch(() => {
+        res.status(400).json({ error: "Enter a unique email" });
+    });
 });
 module.exports = router;
