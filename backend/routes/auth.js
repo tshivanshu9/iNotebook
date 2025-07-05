27500e82985dee46361046b8042c8d9be3463d55
tshivanshu9 <tshivanshu9@gmail.com>
2025-07-05 21:03:27 +0530

    Loing route added

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,6 +7,9 @@ const jwt = require('jsonwebtoken');
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
+/**
+ * @SIGNUP_ROUTE
+ */
 router.post('/create-user', [
     body('name').isLength({ min: 3 }),
     body('email').isEmail(),
@@ -39,4 +42,38 @@ router.post('/create-user', [
         res.status(500).send("Internal Server Error");
     }
 });
+
+/**
+ * @LOGIN_ROUTE
+ */
+router.post('/login', [
+    body('email').isEmail(),
+    body('password').exists(),
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email });
+    if (!user) {
+        return res.status(400).json({ error: "Either of email or password are incorrect" });
+    }
+    const passwordCompare = await bcrypt.compare(password, user.password);
+    if (!passwordCompare) {
+        return res.status(400).json({ error: "Either of email or password are incorrect" });
+    }
+    const data = {
+        user: {
+            id: user._id,
+        }
+    };
+    const token = jwt.sign(data, JWT_SECRET);
+    res.json({ token });
+    } catch (error) {
+        res.status(500).send("Internal Server Error");
+    }
+});
+
 module.exports = router;
